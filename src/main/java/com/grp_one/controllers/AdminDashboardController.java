171084feb6ba671d1ddffbab098f9bb2cc9d28b7
kabi02 <108052970+kabi02@users.noreply.github.com>
2023-02-19 14:11:36 +0800

    first commit

---
+++ src/main/java/com/grp_one/controllers/AdminDashboardController.java
@@ -0,0 +1,243 @@
+package com.grp_one.controllers;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.cell.PropertyValueFactory;
+import com.grp_one.*;
+
+public class AdminDashboardController implements Initializable {
+
+    SqlConnector dbConn = new SqlConnector();
+    public static CustomerInfo selection;
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnView;
+
+    @FXML
+    private TableView<CustomerInfo> dashboardTable;
+
+    @FXML
+    private Label requests;
+    @FXML
+    private Label biometrics;
+    @FXML
+    private Label claiming;
+    @FXML
+    private Label finished;
+
+    @FXML
+    private TableColumn<CustomerInfo, String> ctnColumn;
+    @FXML
+    private TableColumn<CustomerInfo, String> nameColumn;
+    @FXML
+    private TableColumn<CustomerInfo, LocalDate> dateColumn;
+    @FXML
+    private TableColumn<CustomerInfo, String> actionColumn;
+    Dialog<String> dialog = new Dialog<>();
+
+    @FXML
+    private void logoutUser() throws Exception {
+        Admin.setRoot("adminlogin", "Login");
+        Admin.centerRoot();
+        Admin.showStage();
+    }
+
+    @FXML
+    void viewInfo(ActionEvent event) throws Exception {
+        selection = dashboardTable.getSelectionModel().getSelectedItem();
+        if (selection != null) {
+            Admin.setRoot("userinfo_adminside", "Customer Details");
+            Admin.centerRoot();
+            Admin.showStage();
+        }
+    }
+
+    private void initTable() {
+        ctnColumn.setCellValueFactory(new PropertyValueFactory<CustomerInfo, String>("CTN"));
+        ctnColumn.setResizable(false);
+        ctnColumn.setReorderable(false);
+        ctnColumn.setEditable(false);
+
+        nameColumn.setCellValueFactory(new PropertyValueFactory<CustomerInfo, String>("Name"));
+        nameColumn.setReorderable(false);
+        nameColumn.setEditable(false);
+
+        dateColumn.setCellValueFactory(new PropertyValueFactory<CustomerInfo, LocalDate>("Date"));
+        dateColumn.setResizable(false);
+        dateColumn.setReorderable(false);
+        dateColumn.setEditable(false);
+
+        actionColumn.setResizable(false);
+        actionColumn.setReorderable(false);
+        actionColumn.setEditable(false);
+    }
+
+    private void finishedTable() {
+        ObservableList<CustomerInfo> data = FXCollections.observableArrayList();
+        dashboardTable.setItems(null);
+        try {
+            Connection conn = dbConn.dbConn();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            String sql = "SELECT `lname`,`fname`,`mname`, `CTN`, `transaction_date` FROM admin.user_personal_info INNER JOIN admin.application_status WHERE admin.user_personal_info.userID = admin.application_status.userID and admin.application_status.status = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, CustomerInfo.CLAIMED);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                data.add(new CustomerInfo(
+                        rs.getString("lname") + ", " + rs.getString("fname") + " " + rs.getString("mname"),
+                        rs.getString("CTN"),
+                        rs.getDate("transaction_date").toLocalDate()));
+                dashboardTable.setItems(data);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void claimingTable() {
+        ObservableList<CustomerInfo> data = FXCollections.observableArrayList();
+        dashboardTable.setItems(null);
+        try {
+            Connection conn = dbConn.dbConn();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            String sql = "SELECT `lname`,`fname`,`mname`, `CTN`, `transaction_date` FROM admin.user_personal_info INNER JOIN admin.application_status WHERE admin.user_personal_info.userID = admin.application_status.userID and admin.application_status.status = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, CustomerInfo.CLAIMING);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                data.add(new CustomerInfo(
+                        rs.getString("lname") + ", " + rs.getString("fname") + " " + rs.getString("mname"),
+                        rs.getString("CTN"),
+                        rs.getDate("transaction_date").toLocalDate()));
+                dashboardTable.setItems(data);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void biometricsTable() {
+        ObservableList<CustomerInfo> data = FXCollections.observableArrayList();
+        dashboardTable.setItems(null);
+        try {
+            Connection conn = dbConn.dbConn();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            String sql = "SELECT `lname`, `fname`, `mname`, `CTN`, `transaction_date` FROM admin.user_personal_info INNER JOIN admin.application_status WHERE admin.user_personal_info.userID = admin.application_status.userID and admin.application_status.status = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, CustomerInfo.BIOMETRICS);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                data.add(new CustomerInfo(
+                        rs.getString("lname") + ", " + rs.getString("fname") + " " + rs.getString("mname"),
+                        rs.getString("CTN"),
+                        rs.getDate("transaction_date").toLocalDate()));
+                dashboardTable.setItems(data);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void requestsTable() {
+        ObservableList<CustomerInfo> data = FXCollections.observableArrayList();
+        dashboardTable.setItems(null);
+        System.out.println(CustomerInfo.PROCESS);
+        try {
+            Connection conn = dbConn.dbConn();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            String sql = "SELECT `lname`, `fname`, `mname`, `CTN`, `transaction_date` FROM admin.user_personal_info INNER JOIN admin.application_status WHERE admin.user_personal_info.userID = admin.application_status.userID and admin.application_status.status = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, CustomerInfo.PROCESS);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(CustomerInfo.PROCESS);
+                data.add(new CustomerInfo(
+                        rs.getString("lname") + ", " + rs.getString("fname") + " " + rs.getString("mname"),
+                        rs.getString("CTN"),
+                        rs.getDate("transaction_date").toLocalDate()));
+                dashboardTable.setItems(data);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        dialog.setTitle("Dialog");
+        ButtonType type = new ButtonType("Ok", ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().add(type);
+        initTable();
+        requestsTable();
+        requests.setCursor(Cursor.HAND);
+        biometrics.setCursor(Cursor.HAND);
+        finished.setCursor(Cursor.HAND);
+        claiming.setCursor(Cursor.HAND);
+        requests.setOnMouseClicked(event -> {
+            requestsTable();
+        });
+        biometrics.setOnMouseClicked(event -> {
+            biometricsTable();
+        });
+        finished.setOnMouseClicked(event -> {
+            finishedTable();
+        });
+        claiming.setOnMouseClicked(event -> {
+            claimingTable();
+        });
+        btnDelete.setOnMouseClicked(event -> {
+            try {
+                selection = dashboardTable.getSelectionModel().getSelectedItem();
+                if (selection != null) {
+                    Connection conn = dbConn.dbConn();
+                    PreparedStatement stmt = null;
+                    ResultSet rs = null;
+                    String sql = "select admin.application_status.status from admin.application_status where admin.application_status.CTN = ?";
+                    stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, selection.getCTN());
+                    rs = stmt.executeQuery();
+                    rs.next();
+                    System.out.println(rs.getString(1));
+                    if (rs.getString(1).equalsIgnoreCase(CustomerInfo.CLAIMED)) {
+                        sql = "delete from admin.application_status where admin.application_status.CTN = ?";
+                        stmt = conn.prepareStatement(sql);
+                        stmt.setString(1, selection.getCTN());
+                        stmt.executeUpdate();
+                        claimingTable();
+                        dialog.setContentText("Successfully Deleted!");
+                        dialog.showAndWait();
+                    } else {
+                        dialog.setContentText("Application is being Processed, Please Decline to remove from database");
+                        dialog.showAndWait();
+                    }
+                }
+            } catch (Exception e) {
+
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/grp_one/controllers/UInfoAdminController.java
@@ -0,0 +1,190 @@
+package com.grp_one.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+import com.grp_one.*;
+
+public class UInfoAdminController implements Initializable {
+    SqlConnector dbConn = new SqlConnector();
+
+    @FXML
+    private TextField addressCity;
+
+    @FXML
+    private TextField addressCountry;
+
+    @FXML
+    private TextField addressHLB;
+
+    @FXML
+    private TextField addressProv;
+
+    @FXML
+    private TextField addressRFU;
+
+    @FXML
+    private TextField addressStreet;
+
+    @FXML
+    private TextField addressSub;
+
+    @FXML
+    private Button btnAccept;
+    @FXML
+    private Button decline;
+
+    @FXML
+    private TextField regInfoContact;
+
+    @FXML
+    private TextField txtBloodType;
+
+    @FXML
+    private TextField txtDob;
+
+    @FXML
+    private TextField txtFieldCity;
+
+    @FXML
+    private TextField txtFieldCountry;
+
+    @FXML
+    private TextField txtFieldFN;
+
+    @FXML
+    private TextField txtFieldLN;
+    @FXML
+    private Hyperlink linkBack2Dash;
+
+    @FXML
+    private TextField txtFieldMN;
+
+    @FXML
+    private TextField txtFieldProv;
+
+    @FXML
+    private TextField txtIsFilo;
+
+    @FXML
+    private TextField txtMStatus;
+
+    @FXML
+    private TextField txtSex;
+
+    @FXML
+    void backToDash(ActionEvent event) throws Exception {
+        Admin.setRoot("admindashboard", "Admin Dashboard");
+        Admin.centerRoot();
+        Admin.showStage();
+    }
+
+    private void showDeets() {
+        try {
+            Connection conn = dbConn.dbConn();
+            PreparedStatement pst = null;
+            ResultSet rs = null;
+            String query = "SELECT * FROM admin.user_personal_info LEFT JOIN admin.application_status ON admin.user_personal_info.userID = admin.application_status.userID WHERE admin.application_status.CTN = ?;";
+            String query2 = "SELECT * FROM admin.user_address LEFT JOIN admin.application_status ON admin.user_address.userID = admin.application_status.userID WHERE admin.application_status.CTN = ?;";
+            pst = conn.prepareStatement(query);
+            pst.setString(1, AdminDashboardController.selection.getCTN());
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                txtFieldLN.setText(rs.getString("lname").toString());
+                txtFieldFN.setText(rs.getString("fname").toString());
+                txtFieldMN.setText(rs.getString("mname").toString());
+                txtSex.setText(rs.getString("sex").toString());
+                txtDob.setText(rs.getDate("birthdate").toString());
+                txtBloodType.setText(rs.getString("blood_type").toString());
+                txtMStatus.setText(rs.getString("marital_status").toString());
+                txtIsFilo.setText(rs.getString("nationality").toString());
+                txtFieldCity.setText(rs.getString("pob_city"));
+                txtFieldProv.setText(rs.getString("pob_province"));
+                txtFieldCountry.setText(rs.getString("pob_country"));
+                regInfoContact.setText(rs.getString("contact"));
+            }
+            pst = conn.prepareStatement(query2);
+            pst.setString(1, AdminDashboardController.selection.getCTN());
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                addressRFU.setText(rs.getString("unit_no"));
+                addressHLB.setText(rs.getString("house_no"));
+                addressStreet.setText(rs.getString("street"));
+                addressSub.setText(rs.getString("barangay_or_subd"));
+                addressCity.setText(rs.getString("city"));
+                addressProv.setText(rs.getString("province"));
+                addressCountry.setText(rs.getString("country"));
+            }
+
+        } catch (Exception e) {
+            
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        showDeets();
+        btnAccept.setOnMouseClicked(event -> {
+            try {
+                Connection conn = dbConn.dbConn();
+                PreparedStatement stmt = null;
+                ResultSet rs = null;
+                String sql = "select admin.application_status.status from admin.application_status where admin.application_status.CTN = ?";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, AdminDashboardController.selection.getCTN());
+                rs = stmt.executeQuery();
+                rs.next();
+                if (rs.getString(1).equalsIgnoreCase(CustomerInfo.PROCESS)) {
+                    sql = "update admin.application_status set status=? where admin.application_status.CTN = ?";
+                    stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, CustomerInfo.BIOMETRICS);
+                    stmt.setString(2, AdminDashboardController.selection.getCTN());
+                    stmt.executeUpdate();
+                } else if (rs.getString(1).equalsIgnoreCase(CustomerInfo.BIOMETRICS)) {
+                    sql = "update admin.application_status set status=? where admin.application_status.CTN = ?";
+                    stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, CustomerInfo.CLAIMING);
+                    stmt.setString(2, AdminDashboardController.selection.getCTN());
+                    stmt.executeUpdate();
+                } else if (rs.getString(1).equalsIgnoreCase(CustomerInfo.CLAIMING)) {
+                    sql = "update admin.application_status set status=? where admin.application_status.CTN = ?";
+                    stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, CustomerInfo.CLAIMED);
+                    stmt.setString(2, AdminDashboardController.selection.getCTN());
+                    stmt.executeUpdate();
+                }
+                Admin.setRoot("admindashboard", "Admin Dashboard");
+                Admin.centerRoot();
+                Admin.showStage();
+            } catch (Exception e) {
+                
+            }
+        });
+        decline.setOnMouseClicked(event -> {
+            try {
+                Connection conn = dbConn.dbConn();
+                PreparedStatement stmt = null;
+                String sql = "update admin.application_status set status=? where admin.application_status.CTN = ?";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, CustomerInfo.DECLINED);
+                stmt.setString(2, AdminDashboardController.selection.getCTN());
+                stmt.executeUpdate();
+                Admin.setRoot("admindashboard", "Admin Dashboard");
+                Admin.centerRoot();
+                Admin.showStage();
+            } catch (Exception e) {
+
+            }
+        });
+    }
+
+}
